76323b16c9e3dbc464db6333ec1a4038aafc7293
Your Name <youremail@domain.com>
2017-08-22 15:44:49 +0300

    added model for doing the calculations for either multiple or individual product depending

---
+++ app/models/order.rb
@@ -0,0 +1,19 @@
+class Order < ApplicationRecord
+  has_many :order_items
+  before_save :update_total
+  before_create :update_status
+
+  def calculate_total
+    order_items.collect { |item| item.product.price * item.quantity }.sum
+  end
+
+  private
+
+  def update_status
+    self.status = 'In progress' if status == nil?
+  end
+
+  def update_total
+    self.total_price = calculate_total
+  end
+end
