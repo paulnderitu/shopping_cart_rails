7739fc13bb0e23b2ac7138ce19ee917762a4d79f
Your Name <youremail@domain.com>
2017-08-22 13:00:35 +0300

    added code for allowing users to add items to shopping cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,15 @@
 class OrderItemsController < ApplicationController
+  def create
+    @order = current_order
+    @item = @order.order_items.new(item_params)
+    @order.save
+    session[:order_id] = @order.id
+    redirect_to products_path
+  end
+
+  private
+
+  def item_params
+    params.require(:order_item).permit(:quantity, :product_id)
+  end
 end
